96b5e1e093db87232a8d02235f84a3ff689add56
janisarsa <janisarsa@gmail.com>
2023-10-19 21:53:51 -0600

    LiveCoding-1

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // Escribe tu código aquí.
+function bigWords(string,array) {
+    const arreglo=[]
+
+    for (let index = 0; index < array.length; index++) {
+        if (array[index].length > string.length) {
+            arreglo.push(array[index])
+            
+        } 
+        
+    } 
+    const myArray = ['insecto', 'bootcamp', 'mangos', 'reptil', 'mosca', 'escritorio'];
+    bigWords('bocina', myArray)
+    
+}
